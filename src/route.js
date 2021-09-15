8248d6b2e9d1d760385820d4c61cdca1bf583b80
Lucas Cerimeli <lucascerimeli@gmail.com>
2021-09-15 13:04:15 -0300

    Aula 04

--- src/route.js
+++ src/route.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const questionController = require('./controllers/questionController')
+const roomController = require('./controllers/roomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render('room'))
-route.get('/create-room', (req, res) => res.render('create-room'))
+route.get('/', (req, res) => res.render('index', {page: 'enter-room'}))
+route.get('/create-room', (req, res) => res.render('index', {page: 'create-room'}))
+route.get('/room/:room', (req, res) => res.render('room'))
 
 // Formato para o form de dentro da modal passar a informação
- route.post('/room/:room/:question/:action', questionController.index) 
+ route.post('/question/:room/:question/:action', questionController.index) 
+ route.post('/room', roomController.create)
 module.exports = route
